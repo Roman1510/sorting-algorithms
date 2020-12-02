61cc14a3fa1f67cf84d0988c1b583409f1219119
Roman Vinnick <romanvinnick@gmail.com>
2020-12-02 13:39:25 +0600

    added a slided

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
 
 var RandomizeArray = (length, height) => {
   var arr = [];
@@ -8,7 +11,7 @@ var RandomizeArray = (length, height) => {
       <div
         className="line"
         key={index}
-        style={{ height: randomIntFromInterval(1, height) }}
+        style={{ height: randomIntFromInterval(1, height), width: "5px" }}
       ></div>
     );
   }
@@ -18,17 +21,47 @@ function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+  },
+});
+
+function valuetext(value) {
+  //here I can update the value of an array I generate
+  return `${value}`;
+}
 function App() {
-  const [arr, setArr] = useState(RandomizeArray(700, 670));
+  const classes = useStyles();
+  const [arr, setArr] = useState(RandomizeArray(100, 550));
   var RefreshButton = () => {
-    setArr(RandomizeArray(700, 670));
+    setArr(RandomizeArray(100, 550));
   };
 
   return (
     <>
-      <a onClick={() => RefreshButton()} href="#" class="btn btn-1">
-        Refresh
-      </a>
+      <div className="divslider">
+        <a onClick={() => RefreshButton()} href="/#" className="btn btn-1">
+          Refresh
+        </a>
+        <div className={classes.root}>
+          <Typography id="discrete-slider" gutterBottom>
+            Array size
+          </Typography>
+          <Slider
+            defaultValue={30}
+            getAriaValueText={valuetext}
+            aria-labelledby="discrete-slider"
+            valueLabelDisplay="off"
+            step={1}
+            marks
+            min={1}
+            max={15}
+            track={false}
+          />
+        </div>
+      </div>
 
       <div className="graph">{arr}</div>
     </>
